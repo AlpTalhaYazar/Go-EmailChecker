3cc82c09f65dcee7a93fec8cdd2177d62499dc5b
Talha Yazar <talhayazarg@gmail.com>
2022-10-02 15:06:36 +0300

    Created CheckDomain function taking domain as string
    With this function we are checking MX, SPF and DMARC records. If there is any record for these properties of domain we are also printing those records

--- main.go
+++ main.go
@@ -1,4 +1,5 @@
 package main
+
 import (
 	"bufio"
 	"fmt"
@@ -19,4 +20,41 @@ func main() {
 	if err := scanner.Err(); err != nil {
 		log.Fatalf("Error: could not read from input: %v\n", err)
 	}
+}
+
+func checkDomain(domain string) {
+	var hasMX, hasSPF, hasDMARC bool
+	var spfRecord, dmarcRecord string
+
+	mxRecords, err := net.LookupMX(domain)
+
+	if err != nil {
+		log.Printf("Error: could not lookup MX records for %s: %v\n", domain, err)
+	}
+	if len(mxRecords) > 0 {
+		hasMX = true
+	}
+
+	spfRecords, err := net.LookupTXT(fmt.Sprintf("_spf.%s", domain))
+
+	if err != nil {
+		log.Printf("Error: could not lookup TXT records for %s: %v\n", domain, err)
+	}
+	if len(spfRecords) > 0 {
+		hasSPF = true
+		spfRecord = spfRecords[0]
+	}
+
+	dmarcRecords, err := net.LookupTXT(fmt.Sprintf("_dmarc.%s", domain))
+
+	if err != nil {
+		log.Printf("Error: could not lookup TXT records for %s: %v\n", domain, err)
+	}
+	if len(dmarcRecords) > 0 {
+		hasDMARC = true
+		dmarcRecord = dmarcRecords[0]
+	}
+
+	fmt.Printf("\nhasMX : %t\n\nhasSPF : %t\n\nspfRecord : %s\n\nhasDMARC : %t\n\ndmarcRecord : %s\n", hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord)
+
 }
